87af6968a5ad984ec208d798cea08f2980eb4901
joachimdraeger <joachimdraeger@bb8201ed-a7ab-0885-a65a-b5531ee649ba>
2010-11-30 06:49:08 +0000

    removed debug output

--- server/trunk/src/mensa/client.py
+++ server/trunk/src/mensa/client.py
@@ -5,6 +5,5 @@ def get_from_server():
     conn = httplib.HTTPConnection("www.studentenwerk-mannheim.de")
     conn.request("GET", "/mensa/wo_hs.normal.php")
     r1 = conn.getresponse()
-    print r1.status, r1.reason
     data = r1.read()
     return data
