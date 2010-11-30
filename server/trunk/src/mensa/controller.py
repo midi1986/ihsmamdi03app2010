3814591130dde3e0507a0bb1bee38fb7f5677388
joachimdraeger <joachimdraeger@bb8201ed-a7ab-0885-a65a-b5531ee649ba>
2010-11-30 06:49:36 +0000

    controller for mensa

---
+++ server/trunk/src/mensa/controller.py
@@ -0,0 +1,6 @@
+from mensa.client import get_from_server
+from mensa.parser import parseMensa 
+
+print "Content-Type: text/xml; charset=utf-8"
+print ""
+print parseMensa(get_from_server()).asXML()
